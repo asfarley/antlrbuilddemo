21b2b7af522f3e4fbe3afa555d1315a69c5e0ae6
Alexander Farley <alexander.s.farley@gmail.com>
2022-06-02 12:41:38 -0600

    First commit.

---
+++ Dummy.cs
@@ -0,0 +1,39 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using System.Diagnostics;
+
+namespace Compiler
+{
+    public class Dummy
+    {
+        public void Compile(string path)
+        {
+            String input = File.ReadAllText(path);
+            var str = new AntlrInputStream(input);
+            var lexer = new TestlangLexer(str);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new TestlangParser(tokens);
+            //var listener_lexer = new ML2LexerErrorListener<int>();
+            //var listener_parser = new ML2ParserErrorListener();
+            //lexer.RemoveErrorListeners();
+            //parser.RemoveErrorListeners();
+            //parser.RemoveErrorListeners();
+            //lexer.AddErrorListener(listener_lexer);
+            //parser.AddErrorListener(listener_parser);
+            
+            parser.BuildParseTree = true;
+
+            var tree = parser.program_title();
+            Trace.WriteLine(tree.ToStringTree(parser));
+
+            //if (listener_lexer.had_error || listener_parser.had_error)
+            //{
+            //    Trace.WriteLine("Error in parse.");
+            //}
+            //else
+            //{
+            //    Trace.WriteLine("Parse completed.");
+            //}
+        }
+    }
+}
---
+++ Inputs/Testlang.g4
@@ -0,0 +1,15 @@
+﻿grammar Testlang;
+/*
+ * Parser Rules
+ */
+program_title		: TRADENAME PROGRAM NAME eol*;
+eol					: EOL+;
+
+/*
+ * Lexer Rules
+ */
+TRADENAME			: 'TRADENAME';
+PROGRAM				: 'PROGRAM';
+NAME				: [a-zA-Z0-9_.]* ;
+EOL					: ('\n' | '\r\n' | ';') ;
+WHITESPACE          : (' '|'\t')+ -> skip ;
